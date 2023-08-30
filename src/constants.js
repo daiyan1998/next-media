ce94177134dbada44956003df0183e9164ef9f1f
daiyan1998 <abduddaiyan7@gmail.com>
2023-08-30 17:51:09 +0600

    removed quotation from strings in .env variable

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "https://next-media-hfygtegyf-daiyan1998.vercel.app/";
+export const BASE_URL = "";
 export const POSTS_URL = "api/posts";
 export const USERS_URL = "api/users";
